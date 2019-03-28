e53b514e8ea46e5bac87435c411f9f3f81bed624
alinaraza <alina.raza@live.com>
2019-03-28 18:21:20 -0400

    homepage and scss added

---
+++ header.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel = "stylesheet" href ="css/main.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+    <header>
+
+        <!-- navbar -->
+        <nav class="navbar fixed-top navbar-expand-lg bg-primary" id="navbar">
+            <a class="navbar-brand" href="#home">LOGO</a>
+            <button class="navbar-toggler navbar-dark bg-primary" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse nav justify-content-end " id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#location">Location</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    <div id="site-content">
+
