2007f31faf8d6c98cd8e9b010fe385bfa62adffc
alinaraza <alina.raza@live.com>
2019-03-29 15:57:11 -0400

    updated header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel = "stylesheet" href ="css/main.css">
+    <link rel = "stylesheet" href ="http://localhost/Collision-Report/css/main.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
 
