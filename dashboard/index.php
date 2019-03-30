8e9ae616ea41bc0beb1dc2a4adfa8efd6416abcc
nakishaco <nakishacoh@gmail.com>
2019-03-30 01:52:45 -0400

    update

--- dashboard/index.php
+++ dashboard/index.php
@@ -11,7 +11,7 @@
 <body>
   <!-- Navigation -->
     <div class="nav flex-column nav-pills dash navBack" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-      <span class="navbar-brand mb-0"><h3 class="alignTitle">Collision-Report</h3></span>
+      <span class="navbar-brand mb-0"><h2 class="alignTitle">Collision Report</h2></span>
       <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
       <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Report Accident</a>
       <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Account</a>
@@ -21,10 +21,15 @@
 <!-- End of Navigation -->
 
 
-<!-- Home Page -->
-<div class="tab-content content" id="v-pills-tabContent">
 
-  <table class="table table-bordered tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+<div class="tab-content content" id="v-pills-tabContent" role="tabpanel">
+
+<!-- Home Page -->
+<!-- <h2 class="hTitle">Home</h2> -->
+<div class="tab-pane fade active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+  <h1 class="hTitle">Home</h1>
+
+  <table class="table table-bordered tab-pane fade show">
     <thead>
       <tr>
         <th scope="col">Report ID</th>
@@ -44,72 +49,49 @@
         <td>Pending</td>
         <td> <button type="button" class="btn btn-primary">Edit</button></td>
       </tr>
-      <tr>
-        <th scope="row">2</th>
-        <!-- <td>Jacob</td> -->
-        <!-- <td>Thornton</td> -->
-        <td>Upper Middle Road and Trafalgar Road, Oakville</td>
-        <td>Pending</td>
-        <td> <button type="button" class="btn btn-primary">Edit</button></td>
-      </tr>
     </tbody>
   </table>
+</div>
 
   <!-- Report Accident Page -->
   <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
 
   <!-- Account Page -->
   <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
-    <h3>General Account Settings</h3>
+    <h1 class="hTitle">General Account Settings</h1>
 
     <table id="vertical-2">
-               <thead>
-                   <tr>
-                       <th colspan="3">Name</th>
-                   </tr>
-                   <tr>
-                       <th colspan="3">Insurance</th>
-                   </tr>
-                   <tr>
-                       <th colspan="3">Email</th>
-                   </tr>
-                   <tr>
-                       <th colspan="3">Phone Number</th>
-                   </tr>
-               </thead>
-               <tbody>
-                   <tr>
-                       <td>row 1</td>
-                       <td><button type="button" class="btn btn-primary">Edit</button></td>
-                   </tr>
-                   <tr>
-                       <td>PDF</td>
-                       <td><button type="button" class="btn btn-primary">Edit</button></td>
+               <div class="row">
+                 <div class="col-6 col-sm-3"><label for="firstName">Name</label></div>
+                 <div class="col-6 col-sm-3">Joanne Day</div>
+                 <button type="button" class="btn btn-primary">Edit</button>
 
-                   </tr>
-                   <tr>
-                       <td>data</td>
-                       <td><button type="button" class="btn btn-primary">Edit</button></td>
-                   </tr>
-                   <tr>
-                       <td>data</td>
-                       <td><button type="button" class="btn btn-primary">Edit</button></td>
-                   </tr>
-                   <tr>
-                       <td>data</td>
-                       <td><button type="button" class="btn btn-primary">Edit</button></td>
-                   </tr>
-               </tbody>
+                 <!-- Force next columns to break to new line -->
+                 <div class="w-100"></div>
+                 <div class="col-6 col-sm-3"><label for="firstName">Insurance</label></div>
+                 <div class="col-6 col-sm-3">State Farm</div>
+                 <button type="button" class="btn btn-primary">Edit</button>
+
+                 <div class="w-100"></div>
+                 <div class="col-6 col-sm-3"><label for="firstName">Email</label></div>
+                 <div class="col-6 col-sm-3">jd@gmail.com</div>
+                 <button type="button" class="btn btn-primary">Edit</button>
+
+                 <div class="w-100"></div>
+                 <div class="col-6 col-sm-3"><label for="firstName">Phone Number</label></div>
+                 <div class="col-6 col-sm-3">555555555</div>
+                 <button type="button" class="btn btn-primary">Edit</button>
+               </div>
         </table>
+
   </div>
 
   <!-- Contact Page -->
   <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
-    <div  id = "contact" class="bg">
-        <!-- <div class="container"> -->
+    <!-- <div  id = "contact" class="bg"> -->
             <div class = "d-flex flex-wrap">
                 <div class = "col-lg-6 col-sm-12 contact-bkg" >
-                    <h2>Contact</h2>
+                    <h1>Contact</h1>
                     <p>Get in touch by sending a message or calling us at the location closest to you </p>
                     <a href="#location"><button class = "btn-primary btn-lg">Find Location</button></a>
                 </div>
@@ -135,9 +117,8 @@
                         <button type="submit" class="btn btn-primary btn-lg btn-block">Send Message</button>
                     </form>
                 </div>
-            <!-- </div>  -->
         </div>
-    </div>
+    <!-- </div> -->
   </div>
 </div>
 
