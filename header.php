bce264a9c8b7e235958b74d3fe9fd1e6222af8ad
Katie Carson <kate-carson@outlook.com>
2019-03-30 18:44:19 -0400

    Working on implementing bootstrap wizard plugin for form
    Don't have the JavaScript working yet
    Think the HTML is correct

--- header.php
+++ header.php
@@ -1,13 +1,11 @@
-
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel = "stylesheet" href ="http://localhost/Collision-Report/css/main.css">
+    <link rel = "stylesheet" href ="http://localhost/webdev/Collision-Report/css/main.css">
+    <link rel="stylesheet" href="http://localhost/webdev/Collision-Report/css/paper-bootstrap-wizard/paper-bootstrap-wizard.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-
-
 </head>
 
 <body data-spy="scroll" data-target=".navbar" data-offset="50">
@@ -33,5 +31,5 @@
                 </ul>
             </div>
         </nav>
-    <div id="site-content">
 
+    <div id="site-content">
