0e1099a5f5f06e617dde039204176cbcd119cb39
qdcxdz <42975362+qdcxdz@users.noreply.github.com>
2019-04-02 14:46:51 -0400

    update css and index

--- dashboard/index.php
+++ dashboard/index.php
@@ -7,7 +7,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link rel="stylesheet" href="http://cohennak.dev.fast.sheridanc.on.ca/Collision-Report/css/paper-bootstrap-wizard/paper-bootstrap-wizard.css">
+<link rel="stylesheet" href="http://li96.dev.fast.sheridanc.on.ca/Collision-Report/css/paper-bootstrap-wizard/paper-bootstrap-wizard.css">
 
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel = "stylesheet" href ="Collision-Report/css/main.css">
