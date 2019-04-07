0148e65c03bb9b76eeecbc8c0f1d07e2c8b07209
Nakisha <cohennak@sheridancollege.ca>
2019-04-07 12:03:03 -0400

    update

--- dashboard/indexEm.php
+++ dashboard/indexEm.php
@@ -191,10 +191,7 @@
 </body>
 
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<<<<<<< HEAD
-=======
 <script src="http://cohennak.dev.fast.sheridanc.on.ca/Collision-Report/dashboard/script/script.js"></script>
->>>>>>> 5164fd8aa9d79366bf09613766b25526d4080ac8
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
