023d4f0c5ae173751b8bd306a254c6f5949ec315
Katie Carson <kate-carson@outlook.com>
2019-04-07 19:02:22 -0400

    Fixed form link on homepage

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 $page_title = "Home";
 $page = "home";
 
@@ -18,13 +19,13 @@ include 'header.php';
                 </div>
                 <div id= "button-container" class="row  justify-content-center">
                     <div class="col-lg-4 col-md-12 col-sm-12" >
-                        <a href= "http://localhost/Collision-Report/form">
+                        <a href= "http://localhost/Collision-Report/forms">
                             <button type="button" class="btn btn-primary btn-lg btn-block ">
                                 REPORT AN ACCIDENT
                             </button>
                         </a>
                     </div>
-                    <div class= "col-lg-4 col-md-12 col-sm-12"> 
+                    <div class= "col-lg-4 col-md-12 col-sm-12">
                         <a href= "http://localhost/Collision-Report/signIn">
                             <button type="button" class="btn btn-primary btn-lg btn-block">
                                 SIGN IN
@@ -39,14 +40,13 @@ include 'header.php';
 </div>
 
 <!-- About -->
-
 <div id = "about" class="container">
-    <div class= "row"> 
+    <div class= "row">
         <div class= "col text-center">
             <h2>About</h2>
             <p>
                 Logo understands how stressful filing a collision report report can be. Our goal is to
-                make that process easier and more accessible for everyone. 
+                make that process easier and more accessible for everyone.
             </p>
         </div>
     </div>
@@ -54,7 +54,7 @@ include 'header.php';
 
 
 <!-- Contact -->
-<div  id = "contact" class="bg">
+<div  id="contact" class="bg">
     <!-- <div class="container"> -->
         <div class = "d-flex flex-wrap">
             <div class = "col-lg-6 col-sm-12 contact-bkg" >
@@ -62,7 +62,6 @@ include 'header.php';
                 <p>Get in touch by sending a message or calling us at the location closest to you </p>
                 <a href="#location"><button class = "btn-primary btn-lg">Find Location</button></a>
             </div>
-                        
             <div class= "col-lg-6 col-sm-12 formCol">
                 <form action="processes/processing-contact.php" method="POST">
                         <div class="form-group">
@@ -84,12 +83,11 @@ include 'header.php';
                     <button type="submit" class="btn btn-primary btn-lg btn-block">Send Message</button>
                 </form>
             </div>
-        <!-- </div>  -->
-    </div>             
+        </div>
+    <!-- </div> -->
 </div>
 
 <!-- Location -->
-
 <!-- <div class="container justify-content-center" id="home">
     <h2>Find the closet collision center</h2>
     <form class="form-inline my-2 my-lg-0">
