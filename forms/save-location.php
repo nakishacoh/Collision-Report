8d74cb2f680aa7979c63c11d4c12472be4453ce6
Katie Carson <kate-carson@outlook.com>
2019-04-07 13:58:43 -0400

    Working on google maps api

--- forms/save-location.php
+++ forms/save-location.php
@@ -1,41 +1,67 @@
 <?php
-// Connect to database
-$username = "razaalin_alina";
-$password = "iZKoDeSbtiPLYSGT";
-$database = "razaalin_alina-razaalin_collision-report";
+// ***** OUR PHP CODE ***** \\
 
-// Gets data from URL parameters
+// receive values from form
 $name = $_GET['name'];
 $address = $_GET['address'];
 $lat = $_GET['lat'];
 $lng = $_GET['lng'];
 $type = $_GET['type'];
 
-// Opens a connection to a MySQL server
-$connection = mysql_connect ("localhost", $username, $password);
-if (!$connection) {
-    die('Not connected : ' . mysql_error());
-}
+// check admin table for valid username and password
+$dsn = "mysql:host=localhost;dbname=carsokai_collision_report;charset=utf8mb4";
+$dbusername = "carsokai_imm";
+$dbpassword = "AdpteGfKipeuOuJx";
 
-// Sets the active MySQL database
-$db_selected = mysql_select_db($database, $connection);
-if (!$db_selected) {
-    die ('Can\'t use db : ' . mysql_error());
-}
+// connect to database
+$pdo = new PDO($dsn, $dbusername, $dbpassword);
 
-// Inserts new row with place data
-$query = sprintf("INSERT INTO markers " .
-         " (id, name, address, lat, lng, type ) " .
-         " VALUES (NULL, '%s', '%s', '%s', '%s', '%s');",
-         mysql_real_escape_string($name),
-         mysql_real_escape_string($address),
-         mysql_real_escape_string($lat),
-         mysql_real_escape_string($lng),
-         mysql_real_escape_string($type));
+// prepare insert
+$stmt = $pdo->prepare("INSERT INTO `location`(`id`, `name`, `address`, `lat`, `lng`, `type`) VALUES (NULL, '$name', '$address', '$lat', '$lng', '$type')");
 
-$result = mysql_query($query);
+// insert into database
+$stmt->execute();
 
-if (!$result) {
-    die('Invalid query: ' . mysql_error());
-}
+
+// ***** GOOGLE PHP CODE ***** \\
+
+// // Connect to database
+// $username = "carsokai_imm";
+// $password = "AdpteGfKipeuOuJx";
+// $database = "carsokai_imm-carsokai_collision_report";
+//
+// // Gets data from URL parameters
+// $name = $_GET['name'];
+// $address = $_GET['address'];
+// $lat = $_GET['lat'];
+// $lng = $_GET['lng'];
+// $type = $_GET['type'];
+//
+// // Opens a connection to a MySQL server
+// $connection = mysql_connect ("localhost", $username, $password);
+// if (!$connection) {
+//     die('Not connected : ' . mysql_error());
+// }
+//
+// // Sets the active MySQL database
+// $db_selected = mysql_select_db($database, $connection);
+// if (!$db_selected) {
+//     die ('Can\'t use db : ' . mysql_error());
+// }
+//
+// // Inserts new row with place data
+// $query = sprintf("INSERT INTO location " .
+//          " (id, name, address, lat, lng, type ) " .
+//          " VALUES (NULL, '%s', '%s', '%s', '%s', '%s');",
+//          mysql_real_escape_string($name),
+//          mysql_real_escape_string($address),
+//          mysql_real_escape_string($lat),
+//          mysql_real_escape_string($lng),
+//          mysql_real_escape_string($type));
+//
+// $result = mysql_query($query);
+//
+// if (!$result) {
+//     die('Invalid query: ' . mysql_error());
+// }
 ?>
