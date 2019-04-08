7e5a1d17809ac2deaed487b560e4b230ca560fa7
Katie Carson <kate-carson@outlook.com>
2019-04-07 22:33:14 -0400

    Working on homepage and adding google map
    Styling and JavaScript added for the map

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
 
 <footer>
      <div class="d-flex justify-content-end">
-        <a href = "https://instagram.com"><i class="fab fa-instagram fa-2x"></i></a>
-        <a href = "https://twitter.com/"><i class="fab fa-twitter fa-2x"></i></a>
-        <a href = "https://facebook.com"><i class="fab fa-facebook-f fa-2x"></i></a>
+        <a href="https://instagram.com"><i class="fab fa-instagram fa-2x"></i></a>
+        <a href="https://twitter.com/"><i class="fab fa-twitter fa-2x"></i></a>
+        <a href="https://facebook.com"><i class="fab fa-facebook-f fa-2x"></i></a>
     </div>
     <div class="d-flex justify-content-end text-right" id="footer-content">
         <p>For emergency assistance, dial 9-1-1</p>
@@ -12,10 +12,12 @@
 </footer>
 
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<script src= "http://localhost/collision-report/js/main.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="http://localhost/collision-report/js/main.js"></script>
+<script src="http://localhost/collision-report/js/geolocation.js"></script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA7tZk2g1z2Cfj6fLedH8A7c2xF46DLnPE&callback=initMap"></script>
 
 </body>
 
--- index.php
+++ index.php
@@ -9,24 +9,24 @@ include 'header.php';
 
 
 <!-- Home -->
-<div  class = "bg" id="home">
+<div class="bg" id="home">
     <div class="container h-100">
         <div class="row h-100 align-items-center">
             <div class="col-12 text-center" id="home-info">
                 <div id="home-content">
-                    <h1 class= "display-1">LOGO</h1>
+                    <h1 class="display-1">LOGO</h1>
                     <h3>A bit of information about the site</h3>
                 </div>
-                <div id= "button-container" class="row  justify-content-center">
+                <div id="button-container" class="row justify-content-center">
                     <div class="col-lg-4 col-md-12 col-sm-12" >
-                        <a href= "http://localhost/Collision-Report/forms">
+                        <a href="http://localhost/Collision-Report/forms">
                             <button type="button" class="btn btn-primary btn-lg btn-block ">
                                 REPORT AN ACCIDENT
                             </button>
                         </a>
                     </div>
-                    <div class= "col-lg-4 col-md-12 col-sm-12">
-                        <a href= "http://localhost/Collision-Report/signIn">
+                    <div class="col-lg-4 col-md-12 col-sm-12">
+                        <a href="http://localhost/Collision-Report/signIn">
                             <button type="button" class="btn btn-primary btn-lg btn-block">
                                 SIGN IN
                             </button>
@@ -35,68 +35,60 @@ include 'header.php';
                 </div>
             </div>
         </div>
-        </div>
     </div>
 </div>
 
 <!-- About -->
-<div id = "about" class="container">
-    <div class= "row">
-        <div class= "col text-center">
+<div id="about" class="container">
+    <div class="row">
+        <div class="col text-center">
             <h2>About</h2>
             <p>
-                Logo understands how stressful filing a collision report report can be. Our goal is to
-                make that process easier and more accessible for everyone.
+                Logo understands how stressful filing a collision report report can be. Our goal is to make that process easier and more accessible for everyone.
             </p>
         </div>
     </div>
 </div>
 
-
 <!-- Contact -->
-<div  id="contact" class="bg">
-    <!-- <div class="container"> -->
-        <div class = "d-flex flex-wrap">
-            <div class = "col-lg-6 col-sm-12 contact-bkg" >
-                <h2>Contact</h2>
-                <p>Get in touch by sending a message or calling us at the location closest to you </p>
-                <a href="#location"><button class = "btn-primary btn-lg">Find Location</button></a>
-            </div>
-            <div class= "col-lg-6 col-sm-12 formCol">
-                <form action="processes/processing-contact.php" method="POST">
-                        <div class="form-group">
-                            <label for="firstName">First Name</label>
-                            <input type="text" class="form-control"  name="firstname" id="firstname" placeholder="ie.John">
-                        </div>
-                        <div class="form-group">
-                            <label for="lastName">Last Name</label>
-                            <input type="text" class="form-control"  name="lastname" id="lastname" placeholder="ie.Doe">
-                        </div>
-                    <div class="form-group">
-                        <label for="email">Email address</label>
-                        <input type="email" name= "email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
-                    </div>
-                    <div class="form-group">
-                        <label for="message">Message</label>
-                        <textarea class="form-control" name="message" id="message" rows="3"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-lg btn-block">Send Message</button>
-                </form>
-            </div>
+<div id="contact" class="bg">
+    <div class="d-flex flex-wrap">
+        <div class="col-lg-6 col-sm-12 contact-bkg">
+            <h2>Contact</h2>
+            <p>Get in touch by sending a message or calling us at the location closest to you.</p>
+            <a href="#location"><button class="btn-primary btn-lg">Find Location</button></a>
         </div>
-    <!-- </div> -->
+        <div class="col-lg-6 col-sm-12 formCol">
+            <form action="processes/processing-contact.php" method="POST">
+                <div class="form-group">
+                    <label for="firstName">First Name</label>
+                    <input type="text" class="form-control"  name="firstname" id="firstname" placeholder="e.g. John">
+                </div>
+                <div class="form-group">
+                    <label for="lastName">Last Name</label>
+                    <input type="text" class="form-control"  name="lastname" id="lastname" placeholder="e.g. Doe">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" name= "email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label for="message">Message</label>
+                    <textarea class="form-control" name="message" id="message" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary btn-lg btn-block">Send Message</button>
+            </form>
+        </div>
+    </div>
 </div>
 
 <!-- Location -->
-<!-- <div class="container justify-content-center" id="home">
-    <h2>Find the closet collision center</h2>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
-    </form>
-    <div>
-    </div>
-</div> -->
+<div class="container" id="location">
+    <!-- <div class="row">
+        <h2>Find the closest collision center</h2>
+    </div> -->
+    <div class="row" id="map"></div>
+</div>
 
 <?php
 
